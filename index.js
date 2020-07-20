5ee895cddec363e8adfa0e2ad220732c39c63118
Arun Singh <arunnayan.28@gmail.com>
2020-07-20 17:44:44 +0530

    Jstree Done

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ $ = require("jquery");
 require("jstree");
 // $ will take care for jstree also as jstree is built on top ///of jquery
 const nodePath = require("path");
-$(document).ready(function () {
+
+const fs = require("fs");
+
+$(document).ready(async function () {
   let currentPath = process.cwd();
 
   let data = [];
@@ -11,13 +14,54 @@ $(document).ready(function () {
     parent: "#",
     text: getFolderNameFromPath(currentPath),
   };
+  let rootChildren = getCurrentDirectories(currentPath);
+
+  data = data.concat(rootChildren);
+
   data.push(baseObject);
-  $("#jstree").jstree({
-    core: {
-      data: data,
-    },
-  });
+
+  $("#jstree")
+    .jstree({
+      core: {
+        // so that create works
+        check_callback: true,
+        data: data,
+      },
+    })
+    .on("open_node.jstree", function (e, data) {
+      //console.log(data.node.children);
+      data.node.children.forEach(function (child) {
+        let childDirectories = getCurrentDirectories(child);
+
+        for (let i = 0; i < childDirectories.length; i++) {
+          let granchildren = childDirectories[i];
+          $("#jstree").jstree().create_node(child, granchildren, "last");
+        }
+      });
+    });
 });
+
+function getCurrentDirectories(path) {
+  if (fs.lstatSync(path).isFile()) {
+    return [];
+  }
+
+  let files = fs.readdirSync(path);
+  // console.log(files);
+
+  let rv = [];
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i];
+
+    rv.push({
+      id: nodePath.join(path, file),
+      parent: path,
+      text: file,
+    });
+  }
+
+  return rv;
+}
 
 function getFolderNameFromPath(path) {
   return nodePath.basename(path);
